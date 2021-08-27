5ca84690c79f89bd67ca2b42f87b651638dc153e
LambFerret <dignrjrl12@gmail.com>
2021-08-27 22:45:22 +0900

    20210827 ver2

--- routes/register.js
+++ routes/register.js
@@ -1,8 +1,15 @@
-var router = require('express').Router();
+const express = require('express')
+const app = express()
+const router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('register', { title: 'this is register form' });
 });
 
+app.use(express.urlencoded({ extended: false}))
+app.post('/process-contact',(req, res)=>{
+    console.log(`-------------received ${req.body.name}`)
+    res.send('hi')
+})
 module.exports = router;
