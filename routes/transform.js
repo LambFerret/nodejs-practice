c6ee075811a2b2e4dc85023bc142ae1676c82ecc
LambFerret <dignrjrl12@gmail.com>
2021-09-14 15:45:25 +0900

    Update transform.js

--- routes/transform.js
+++ routes/transform.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { rand } = require('@tensorflow/tfjs-core');
 const multer = require("multer")
 const upload = multer({ dest: 'images/' })
 //const model = require("../lib/transformModel")
@@ -9,10 +10,12 @@ var beforePic = "models/imgs/fall wallpaper4.jpg"
 router.post('/', upload.single("imgArray"), (req, res) => {
     res.json(req.file)
     console.log(req.file);
+    var date = new Date()
+    var randn = date.getTime()
     var filename = 2
     var origin = req.body.origin;
     var convert = req.body.convert;
-    var ls = [filename, origin, convert]
+    var ls = [randn, origin, convert]
     db.insertRow("UploadImg", ls)
 
 })
