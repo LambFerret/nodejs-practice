d7cdf981a05a6b342d97ff0d0d720cb365711d7b
LambFerret <dignrjrl12@gmail.com>
2021-09-14 09:07:42 +0900

    upload file test code

--- routes/transform.js
+++ routes/transform.js
@@ -1,26 +1,32 @@
 const router = require('express').Router();
 const multer = require("multer")
-const upload = multer({dest:'images/'})
+const upload = multer({ dest: 'images/' })
 //const model = require("../lib/transformModel")
+const db = require("../data/mariaDBdatabase")
 
 var beforePic = "models/imgs/fall wallpaper4.jpg"
 
-router.post('/',upload.single("imgArray"),(req, res)=>{
+router.post('/', upload.single("imgArray"), (req, res) => {
     res.json(req.file)
     console.log(req.file);
+    var filename = 2
+    var origin = req.body.origin;
+    var convert = req.body.convert;
+    var ls = [filename, origin, convert]
+    db.insertRow("UploadImg", ls)
 
 })
 router.get('/', function (req, res) {
     if (!req.session.passport) res.redirect("/")
     // model.prediction(0, true, beforePic)
     //     .then((value, err) => (
-            res.render('transform', {
-                title: 'Transform',
-                before: beforePic,
-    id : req.session.passport.user,
-    after: beforePic,
-            })
-//         ))
+    res.render('transform', {
+        title: 'Transform',
+        before: beforePic,
+        id: req.user,
+        after: beforePic,
+    })
+    //         ))
 });
 
 module.exports = router;
