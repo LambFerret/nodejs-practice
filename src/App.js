a9636c4735a307c583942a2760b8627b62b1e418
LambFerret <dignrjrl12@gmail.com>
2021-09-19 16:00:30 +0900

    react 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
 import './App.css';
 
 function App() {
-  return (
+  var isEmpty = false;
+  var result;
+  if (isEmpty) {
+    result = <p><a href="/">BACK TO HOME</a></p>
+  }
+  else {
+    result = <Footer></Footer>
+  }
+    return (
     <div className="App">
-      hey! i am basic!
+      <Header name="namehere" title="titlehere"></Header>
+  <Body></Body>
+  if (isEmpty) {
+    result = <p><a href="/">BACK TO HOME</a></p>
+  }
+  else {
+    result = <Footer></Footer>
+  }
     </div>
   );
 }
 
+function Header(props){
+  return  <h1>Hello {props.name}! you are here {props.title} !</h1>
+}
+
+function Body(){
+  return null
+}
+
+function Footer() {
+  var isLoggined = true
+  if (isLoggined) {
+    return (
+      <p className="footer">
+        <ul>
+          <li><a href="/logout">Logout</a></li>
+          <li><a href="/transform">transform</a></li>
+          <li><a href="/profile">profile</a></li>
+          <li><a href="/community/page/1?number=10">community</a></li>
+        </ul>
+      </p>
+
+    )
+  } else {
+    return (
+      <p className="footer">
+        <ul>
+          <li><a href="/login">Login</a></li>
+          <li><a href="/register">Register</a></li>
+        </ul>
+      </p>
+    )
+  }
+
+}
 export default App;
