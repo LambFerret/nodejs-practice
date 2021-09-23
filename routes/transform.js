e1509d8101c40cff6eb233a6cc037919c3098234
LambFerret <dignrjrl12@gmail.com>
2021-09-23 23:21:30 +0900

    20210923

--- routes/transform.js
+++ routes/transform.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const fs = require("fs")
-//const model = require("../lib/transformModel")
+const model = require("../lib/transformModel")
 const db = require("../data/mariaDBdatabase")
 
 var beforePic = "models/imgs/fall wallpaper4.jpg"
@@ -11,25 +11,29 @@ router.post('/', (req, res) => {
     var imgArray = req.body.imgArray;
     var file = `public/images/${now}.txt`
     console.log(file);
-    fs.writeFile(file,imgArray, 'utf-8',(err,fd)=>{
+    fs.writeFile(file, imgArray, 'utf-8', (err, fd) => {
         if (err) throw err;
-        console.log(file+"위치 확인 바람");
+        console.log(file + "위치 확인 바람");
     })
     var origin = req.body.origin;
     var convert = req.body.convert;
     console.log(origin + convert);
 
 })
+
+
+
+
 router.get('/', function (req, res) {
-    // model.prediction(0, true, beforePic)
-    //     .then((value, err) => (
-    res.render('transform', {
-        title: 'Transform',
-        before: beforePic,
-        id: req.user,
-        after: beforePic,
-    })
-    //         ))
+    model.prediction(0, true, beforePic)
+        .then((value, err) => (
+            res.render('transform', {
+                title: 'Transform',
+                before: beforePic,
+                id: req.user,
+                after: value,
+            })
+        ))
 });
 
 module.exports = router;
