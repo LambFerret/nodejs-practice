9100a88e99d4554c63306c7fd179442f378a2e21
LambFerret <83396036+LambFerret@users.noreply.github.com>
2021-09-27 17:35:02 +0900

    images

--- routes/transform.js
+++ routes/transform.js
@@ -4,27 +4,35 @@ const db = require("../data/mariaDBdatabase");
 // image upload
 const multer = require("multer")
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/uploads/')
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname)
-  }
+    destination: (req, file, cb) => {
+        cb(null, 'public/uploads/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
 })
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage, limits:{fileSize: 3*1024*1024 } })
 
-router.post("/", upload.single("image"), (req, res) => {
-  var id = 'asdfasdf'//req.body.id
-  var origin = req.body.origin
-  var convert = req.body.convert
-  console.log(req.file)
+router.get("/",(req, res)=>{
+    res.render("mutl")
+})
+
+router.post("/", upload.single("image"), async (req, res) => {
+    var id = 'asdfasdf'//req.body.id
+    var origin = req.body.origin
+    var convert = req.body.convert
+    var dataset = `${origin}2${convert}`
+    var filename = `${id}_${dataset}`
+    // var filenumber =await db.getMaxCount()
+    res.send({'asdf':'asdf'})
+    console.log(req.file)
 })
 // --> image upload
 
 
 
 router.get('/', function (req, res) {
-    res.send({hi:"HI"})
+    res.send({ hi: "HI" })
 });
 
 module.exports = router;
