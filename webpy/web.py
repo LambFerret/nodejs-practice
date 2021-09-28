1df246683fd2f99b2a00e2b15b8dc48ebd6b7b4d
LambFerret <83396036+LambFerret@users.noreply.github.com>
2021-09-28 17:50:27 +0900

    Update web.py

--- webpy/web.py
+++ webpy/web.py
@@ -9,30 +9,18 @@ app = FastAPI()
 
 
 def prediction(dataset, imgpath):
-    model = keras.models.load_model("./" + dataset)
+    model = keras.models.load_model("./dataset/" + dataset)
     img = Image.open(imgpath).resize((256, 256))
     img = np.uint8(img) / 127.5 - 1
     model.predict(img)
 
 
-class Imgs(BaseModel):
-    UserID: str
-    origin: str
-    convert: str
-
-
-@app.post("/convert")
-async def read_root(img_info: Imgs):
-    # image 받으면 origin image로 저장
-    userid = img_info.UserID
-    ori = img_info.origin
-    conv = img_info.convert
-    dataset = ori + "2" + conv
-    prediction(dataset, )
-    return {"img_id": f'{userid}_{ori}_{conv}'}
+@app.get("/convert")
+async def convert(dataset: str, imgname: str):
+    str1 = f'{dataset}_{imgname}'
+    print(str1)
+    return {"img_id": str1}
 
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=9889)
-
-#  pip install -r requirements.txt
