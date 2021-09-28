59d1eab7a5ae87a0bee3fc9eb8d6682115340a40
LambFerret <83396036+LambFerret@users.noreply.github.com>
2021-09-28 14:19:33 +0900

    Update transform.js

--- routes/transform.js
+++ routes/transform.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const fs = require("fs")
 const db = require("../data/mariaDBdatabase");
 const axios = require('axios')
-// image upload
 const multer = require("multer");
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -17,7 +16,6 @@ const upload = multer({ storage: storage, limits: { fileSize: 3 * 1024 * 1024 } 
 router.get("/", async (req, res) => {
     var count = await db.getCount("UPLOADIMG", "dataset", "id")
     console.log(count[0].ctd);
-
 })
 
 router.post("/",
@@ -25,25 +23,30 @@ router.post("/",
     async (req, res) => {
         console.log(req.body);
         console.log(req.file);
+        var originFileName = req.file.originalname
         var id = req.body.userInfo
         var origin = req.body.origin
         var convert = req.body.convert
         var dataset = `${origin}2${convert}`
-        var count = await db.getCount("UPLOADIMG", dataset, id)
-        var filename = `${id}_${dataset}_${count[0].ctd}`
-        // try{
-        //     db.insertRow("UPLOADIMG", [count, id, filename, dataset])
-        //     fetch(`http://localhost:9889/convert?dataset=${dataset}&imgname=${filename}`, { method: "get" })
+        await db.getCount("UPLOADIMG", dataset, id).then((count)=>
+            {
+                var filename = `${id}_${dataset}_${count[0].ctd}.jpg`
+            fs.rename(originFileName, filename)
+            // try{
+            //     db.insertRow("UPLOADIMG", [count, id, filename, dataset])
+            //     fetch(`http://localhost:9889/convert?dataset=${dataset}&imgname=${filename}`, { method: "get" })
+    
+            // }
+            // catch (err) {if (err) console.error(err);}
 
-        // }
-        // catch (err) {if (err) console.error(err);}
+    })
 
 
         // var filenumber =await db.getMaxCount()
         res.send({ 'asdf': 'asdf' })
         console.log(filename)
     })
-// --> image upload
+
 
 
 
