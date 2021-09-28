8b85092fc296a65feea2414ad3480b0af960b3c8
LambFerret <83396036+LambFerret@users.noreply.github.com>
2021-09-28 17:49:18 +0900

    20210928 model <promise>

--- routes/transform.js
+++ routes/transform.js
@@ -12,23 +12,19 @@ const storage = multer.diskStorage({
         cb(null, file.originalname)
     }
 })
-const upload = multer({ storage: storage, limits: { fileSize: 3 * 1024 * 1024 } })
+const upload = multer({ storage: storage, limits: { fileSize: 10 * 1024 * 1024 } })
 
 router.post("/",
     upload.single("image"),
     async (req, res) => {
-        console.log(req.body);
-        console.log(req.file);
         var id = req.body.userInfo
         var origin = req.body.origin
         var convert = req.body.convert
         var dataset = `${origin}2${convert}`
         var count = await db.getCount("UPLOADIMG", dataset, id)
-        console.log(count);
         count = count[0].ctd
         var filename = `${id}_${dataset}_${count}.jpg`
         var realpaths = req.file.originalname
-        console.log(filename);
         redUrl = `http://localhost:9889/convert?dataset=${dataset}&imgname=${realpaths}`
         setTimeout(()=>{
             db.insertRow("UPLOADIMG", [count, id, filename, dataset, realpaths])
