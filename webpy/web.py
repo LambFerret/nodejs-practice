e6e0e3e1be2fb1b6f27c8ee6c89977144bbcfccb
LambFerret <dignrjrl12@gmail.com>
2021-09-30 05:48:40 +0900

    end with python script

--- webpy/web.py
+++ webpy/web.py
@@ -1,26 +1,40 @@
+import os
 from fastapi import FastAPI
-from pydantic import BaseModel
 import keras
 import numpy as np
 from PIL import Image
 import uvicorn
+import matplotlib.pyplot as plt
 
 app = FastAPI()
 
 
-def prediction(dataset, imgpath):
-    model = keras.models.load_model("./dataset/" + dataset)
-    img = Image.open(imgpath).resize((256, 256))
+def prediction(dataset, imgname):
+
+    model = keras.models.load_model("webpy/datasets/" + dataset)
+    img = Image.open("webpy/uploads/"+imgname).resize((256, 256))
     img = np.uint8(img) / 127.5 - 1
-    model.predict(img)
+    img = img[np.newaxis, :, :, :]
+    output = model.predict(img)
+    output = 0.5 * output + 0.5
+    plt.imshow(output[0])
+    endpoint = 'webpy/converts/'+imgname
+    plt.savefig(endpoint)
+    plt.close()
+    return endpoint
 
 
 @app.get("/convert")
 async def convert(dataset: str, imgname: str):
+    prediction(dataset, imgname+'.jpg')
     str1 = f'{dataset}_{imgname}'
-    print(str1)
     return {"img_id": str1}
 
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=9889)
+
+# pip install -r /webpy/requirements.txt
+
+# start with..
+# python webpy/web.py
