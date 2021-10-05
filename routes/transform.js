138e470579b40477a54ee86552509f3340e4df11
LambFerret <dignrjrl12@gmail.com>
2021-10-06 00:17:51 +0900

    savepoint

--- routes/transform.js
+++ routes/transform.js
@@ -15,49 +15,57 @@ const upload = multer({ storage: storage, limits: { fileSize: 10 * 1024 * 1024 }
 
 router.post("/",
     upload.single("image"),
-    async (req, res) => {
+     (req, res) => {
         var id = req.body.userInfo
         var origin = req.body.origin
         var convert = req.body.convert
         var dataset = `${origin}2${convert}`
         dataset = dataset.toLowerCase()
-        var count = await db.getCount("UPLOADIMG", dataset, id)
+        var count = db.getCount("UPLOADIMG", dataset, id)
         count = count[0].ctd
         var filename = `${id}_${dataset}_${count}.jpg`
         var realpaths = req.file.originalname.split('.')[0]
         redUrl = `http://localhost:9889/convert`
         db.insertRow("UPLOADIMG", [count, id, filename, dataset, realpaths])
-        await axios.get(redUrl, {
+         axios.get(redUrl, {
             params: {
                 dataset: dataset,
                 imgname: realpaths,
                 imgID: filename,
             }
         })
-            .then(async (v) => {
+            .then((v) => {
                 var convID = v.data.img_id
                 db.insertRow("CONV_IMG", [convID.split('/')[1], filename])
+            }).then(async()=>{
                 imageRows = await db.useWisely(`
                 Select a.*, b.UserID
                 from CONV_IMG a left outer join UPLOADIMG b
                 on a.Up_Img_ID = b.Up_Img_Nm where UserID = '${req.user.id}'
-                `)                
+                limits 1
+                `)      
+
                 config(req, res, "transform", {
-                    afterImgs: imageRows,
+                    afterImgs: imageRows[0],
                 })
+
             })
     })
+// 위에 axios가 동기적으로 페이지 오픈하도록
 
-
+// 쿼리문에 limit 1 이 최신꺼가 뜨던지 아니면 
 router.get('/', async (req, res) =>{
     imageRows = await db.useWisely(`
     Select a.*, b.UserID
     from CONV_IMG a left outer join UPLOADIMG b
     on a.Up_Img_ID = b.Up_Img_Nm where UserID = '${req.user.id}'
+    limit 1
+
     `)
     console.log(imageRows[0]);
     config(req, res, "transform", {
-        afterImgs: imageRows,
+        afterImgs: imageRows[0],
     })
 });
+
 module.exports = router;
