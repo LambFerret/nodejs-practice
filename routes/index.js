a218d2dc5139f9bce210291be85eebb529bdbbfe
LambFerret <83396036+LambFerret@users.noreply.github.com>
2021-10-07 16:48:57 +0900

    end of the line

--- routes/index.js
+++ routes/index.js
@@ -12,42 +12,14 @@ router.get("/logout", (req, res) => {
 router.get("/admin", async (req, res) => {
   var queryss = `
 
-select * from UPLOADIMG
+  Select a.*, b.UserID, b.realpath
+from CONV_IMG a left outer join UPLOADIMG b
+on a.Up_Img_ID = b.Up_Img_Nm where UserID = 'asdfasdf'
+order by left(Conv_Img_ID,8) asc
+limit 1;
   
   ;`
-  // select * from UPLOADIMG
-  // insert into POSTING values ("1633145371658asdfasdf_spring2autumn_0.jpg",'this33222 text',0,'winter','qwerqwer',0, null)
-  // insert into UPLOADIMG values (0, "asdfasdf", "asdfasdf_spring2summer_0.jpg","spring2summer")
-  // ALTER TABLE UPLOADIMG MODIFY Up_Img_Method varchar(20)
-  // alter table CONV_IMG add constraint CONV_IMG_ibfk_1 foreign key(Up_Img_ID) references UPLOADIMG (Up_Img_Nm)
-  // alter table UPLOADIMG add primary key(Up_Img_Nm);
-  // alter table CONV_IMG drop foreign key CONV_IMG_ibfk_1
-  // select * from POSTING
-  //조인조인
-  // select a.*, b.Post_Count, c.Comment_Count
-  // from POSTING a, (SELECT PostID, count(*) as Post_Count from POSTLIKE group by PostID) b, (SELECT PostID, count(*) as Comment_Count from COMMENT group by PostID) c
-  // where a.PostID = b.PostID AND a.PostID = c.PostID
-  // group by a.PostID
-
-
-
-  // SELECT * FROM USER_CONS_COLUMNS WHERE TABLE_NAME = 'POSTING'; 
-  // alter table POSTING modify PostNum auto_increment
-  // insert into (select PostNum from POSTING where PostID = 1) values 1
-  // alter table POSTING add PostNum int
-  // insert into COMMENT(UserID, PostID, Comment_Text) VALUES("asdfasdf", 1, "nice picture dude hahaha")
-  // insert into POSTLIKE(UserID,PostID) VALUES("qwerqwer", 4)
-  // insert into POSTING values(4, null, null, "winter", "asdfasdf", null);
-  // drop table COMMENT
-  // CREATE TABLE COMMENT (    UserID varchar(30) NOT NULL,    PostID int NOT NULL,    Comment_Text varchar(255) NOT NULL, Comment_PK int AUTO_INCREMENT,    PRIMARY KEY (Comment_PK),    FOREIGN KEY (UserID) REFERENCES USER(UserID),    FOREIGN KEY (PostID) REFERENCES POSTING(PostID));
-  // CREATE TABLE POSTLIKE (  UserID varchar(30) NOT NULL,    PostID int NOT NULL,   LikeNum int AUTO_INCREMENT,   PRIMARY KEY (LikeNum),    FOREIGN KEY (UserID) REFERENCES USER(UserID),    FOREIGN KEY (PostID) REFERENCES POSTING(PostID));
-  // delete from UPLOADIMG where UserID="asdfasdf"
-  //COMMENT, CONV_IMG, POSTING, POSTLIKE, UPLOADIMG, USER
-
-  // select * from POSTING as A 
-  // left outer join 
-  // (select PostID as fkid FROM COMMENT)
-  //  as B on A.UserID = B.fkid
+  
 
   rows = await db.useWisely(queryss)
   res.send(JSON.stringify(rows, null, /\n/))
