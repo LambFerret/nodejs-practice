e1d262ce10591d1f2fcbb7b746e72157b80d539c
LambFerret <dignrjrl12@gmail.com>
2021-10-11 09:27:23 +0900

    Update web.py

--- webpy/web.py
+++ webpy/web.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-import keras
+# import keras
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
@@ -7,22 +7,22 @@ import datetime
 
 app = FastAPI()
 
-def prediction(dataset, imgname, imgID):
-    model = keras.models.load_model("datasets/" + dataset)
-    img = Image.open("uploads/"+imgname).resize((256, 256))
-    img = np.uint8(img) / 127.5 - 1
-    img = img[np.newaxis, :, :, :]
-    output = model.predict(img)
-    output = 0.5 * output + 0.5
-    t = datetime.datetime.now()
-    a = (t-datetime.datetime(2021,1,1)).total_seconds()
-    a = str(int(a))
-    plt.imshow(output[0])
-    plt.axis("off")
-    endpoint = "converts/"+a+imgID
-    plt.savefig(endpoint)
-    plt.close()
-    return endpoint
+# def prediction(dataset, imgname, imgID):
+#     model = keras.models.load_model("datasets/" + dataset)
+#     img = Image.open("uploads/"+imgname).resize((256, 256))
+#     img = np.uint8(img) / 127.5 - 1
+#     img = img[np.newaxis, :, :, :]
+#     output = model.predict(img)
+#     output = 0.5 * output + 0.5
+#     t = datetime.datetime.now()
+#     a = (t-datetime.datetime(2021,1,1)).total_seconds()
+#     a = str(int(a))
+#     plt.imshow(output[0])
+#     plt.axis("off")
+#     endpoint = "converts/"+a+imgID
+#     plt.savefig(endpoint)
+#     plt.close()
+#     return endpoint
 
 @app.get("/convert")
 def convert(dataset: str, imgname: str, imgID: str):
